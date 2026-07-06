b8419af0491897ccab8e4ffe053e6b3a5e3d655f
Ouattara Bitcheresse Issouf <issouf@192.168.68.54>
2026-07-06 18:12:32 +0000

    fixed: port au niveau de worflow 22

--- backend/pnpm.config.cjs
+++
@@ -1,4 +0,0 @@
-/** @type {import('pnpm').PnpmConfig} */
-module.exports = {
-  onlyBuiltDependencies: ['@prisma/engines', 'esbuild', 'prisma'],
-}
--- backend/src/types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import type { AuthPayload } from "../middlewares/auth.ts";
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: AuthPayload;
-    }
-  }
-}
